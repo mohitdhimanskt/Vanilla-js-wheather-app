ca7ed824fcd4f1d71ea8c042bde5d720fb328bae
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-08 15:04:43 +0530

    Eightcommit

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',()=> {
+    let long;
+    let lat;
+    let tempdegree = document.querySelector('.tempdegree');
+    let tempdescription = document.querySelector('.tempdescription');
+    let locationtimezone = document.querySelector('.location-timezone');
+})
